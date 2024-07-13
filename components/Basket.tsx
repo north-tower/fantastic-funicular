512a5aa5584f7d675dd4e58c8c3370e055131098
north-tower <muneshmyke@gmail.com>
2024-07-13 15:43:16 +0300

    12

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -4,6 +4,7 @@ import { getCartTotal } from "@/lib/getCartTotal"
 import { groupBySKU } from "@/lib/groupBySKU"
 import { useCartStore } from "@/store"
 import Image from "next/image";
+import AddToCart from "./AddToCart";
 
 
 function Basket() {
@@ -12,14 +13,15 @@ function Basket() {
     const basketTotal = getCartTotal(cart);
 
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-4xl mx-auto">
         <ul className="space-y-5 divide-y-2">
             {Object.keys(grouped).map((sku => {
                 const item = grouped[sku][0]
                 const total = getCartTotal(grouped[sku]);
 
                 return(
-                    <li key={sku} className="p-5 my-2 flex items-center justify-between">
+                    <li key={sku} 
+    className="p-5 my-2 flex items-center justify-between">
                         {item.images[0] && (
                             <Image src={item.images[0]}
                                 alt={item.title}
@@ -27,6 +29,22 @@ function Basket() {
                                 height={100}
                                 />
                         )}
+                        <div className="flex space-x-4 pl-4">
+                            <div>
+                                <p className="line-clamp-2 font-bold">
+                                    {item.title}
+                                </p>
+                                <div 
+                                dangerouslySetInnerHTML={{ __html: item.description}}
+                                className="line-clamp-1 font-light text-sm mt-2"
+                                />                             
+                            </div>
+                            <div className="flex flex-col border rounded-md p-5">
+                                <AddToCart product={item} />
+
+                                <p className="mt-4 font-bold text-right">{total}</p>
+                            </div>
+                        </div>
                     </li>
                 );
             }))}
